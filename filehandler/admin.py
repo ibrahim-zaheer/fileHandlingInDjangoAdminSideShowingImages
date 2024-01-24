1390395b99fbe8f8064fa63c70c2bfdc5a4be02d
Ibrahim-Za <fa21-bcs-009@cuilahore.edu.pk>
2024-01-24 13:14:53 +0500

    making changes

--- filehandler/admin.py
+++ filehandler/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import ProfileData
+
+
+# add this class here to display values in admin side
+
+class ProfileDataAdmin(admin.ModelAdmin):
+    list_display = ('name','profileFile')
+
 # Register your models here.
-admin.site.register(ProfileData)
+admin.site.register(ProfileData,ProfileDataAdmin)
--- filehandler/views.py
+++ filehandler/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ProfileData
 # Create your views here.
 
+#use this view function to get the models and then accessing their data
+
 def hello(request):
-    pass
+    profile_queryset = ProfileData.objects.all()
+    return render(request,"home.html",{"profile_queryset":profile_queryset})
